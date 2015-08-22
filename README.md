08546202b84b00ab6a4f9bb791094bd396f7576f
Jose Grijalva <karlosgg@gmail.com>
2015-08-21 23:40:06 -0600

    final

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quiz-desde_cero
+Proyecto Quiz realizado desde cero, siguiendo los pasos vistos en clase
+
+# Autor
+José Carlos Grijalva González
+
+#Ver en funcionamiento
+https://jc-quiz-2015.herokuapp.com/
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz', errors: [] });
+});
+
+// Autoload de comandos :quizId
+router.param('quizId', quizController.load);
+
+/* GET author page */
+
+router.get('/author', function(req, res) {
+  res.render('author', { author: 'José Carlos Grijalva Gonzalez', errors:[] });
+});
+
+/*GET Quizes*/
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy)
+
+module.exports = router;
